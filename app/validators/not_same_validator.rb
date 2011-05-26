9996e0c37617f389c5ed1f72c5e713d69b420545
macournoyer <macournoyer@gmail.com>
2011-05-26 13:35:36 -0400

    Remove Thin dep. Implement custom validator.

---
+++ app/validators/not_same_validator.rb
@@ -0,0 +1,8 @@
+class NotSameValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    as = options[:as]
+    if value == record.send(as)
+      record.errors.add attribute, "should not be the same as #{as}"
+    end
+  end
+end
