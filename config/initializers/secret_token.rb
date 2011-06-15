379e7b6e3a6091942f2f8b08f759ccf3a3d7978b
macournoyer <macournoyer@gmail.com>
2011-06-15 16:13:24 -0400

    Setting up app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Owning::Application.config.secret_token = 'afa2be3270f2c4a73569d8c07f55aabee4183784f0122f5d0d8917ef4971f08d7a372751fd7099c2c7f354c512cc9aa1da38d1ba0c828c16210fd37dd6e26cfb'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Owning::Application.routes.draw do
+  devise_for :users
+  
+  root :to => 'home#index'
+end
