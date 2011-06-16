c0177ba5ee6030566fff95600b7f0d9aed5d6ae2
macournoyer <macournoyer@gmail.com>
2011-06-16 14:58:10 -0400

    Implement FlashResponder

--- app/responders/flash_responder.rb
+++ app/responders/flash_responder.rb
@@ -1,6 +1,8 @@
 module FlashResponder
   def to_html
-    # ...
+    if post? && !has_errors?
+      controller.flash[:success] = controller.controller_name.humanize + " created!"
+    end
     super
   end
 end
