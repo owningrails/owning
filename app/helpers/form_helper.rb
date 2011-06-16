1659e1a5f218b3c39a9413354ee835a52411472e
macournoyer <macournoyer@gmail.com>
2011-06-16 15:36:09 -0400

    Refactor our form using FormBuilder.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -5,6 +5,24 @@ module FormHelper
   end
   
   class LabellingFormBuilder < ActionView::Helpers::FormBuilder
+    %w( text_field text_area ).each do |type|
+      class_eval <<-RUBY
+        def #{type}(name, *)
+          @template.content_tag :div, label(name) + @template.tag(:br) + super, :class => "field"
+        end
+      RUBY
+    end
     
+    def actions(&block)
+      content = submit
+      content << @template.capture(&block) if block
+      @template.content_tag :div, content, :class => "actions"
+    end
+    
+    def errors
+      if object.errors.any?
+        @template.render "form/errors", :object => @object
+      end
+    end
   end
 end
