fab1374d59cd82836b8c24fd60bd387ebeb92926
macournoyer <macournoyer@gmail.com>
2011-06-16 13:49:57 -0400

    Add custom validator.

--- test/unit/ticket_test.rb
+++ test/unit/ticket_test.rb
@@ -4,4 +4,9 @@ class TicketTest < ActiveSupport::TestCase
   test "creation" do
     assert_valid Ticket.create(:title => "Holy wow!")
   end
+  
+  test "custom validation" do
+    assert_present Ticket.create(:title => "Same", :description => "Same").errors[:description]
+    assert_blank Ticket.create(:title => "Same", :description => "Not same").errors[:description]
+  end
 end
