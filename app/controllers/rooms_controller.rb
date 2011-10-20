b55182b95f31e937f8d06bc82a762273dcd703fe
macournoyer <macournoyer@gmail.com>
2011-10-20 15:19:55 -0400

    Add scaffold views to rooms.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,11 +1,53 @@
 class RoomsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_room, :only => [:show, :edit, :update, :destroy]
+  
+  respond_to :html, :json
   
   def index
-    @rooms = current_user.account.rooms
+    @rooms = rooms
+    
+    respond_with @rooms
   end
   
   def show
-    @room = current_user.account.rooms.find(params[:id])
+    respond_with @room
   end
+  
+  def new
+    @room = Room.new
+    
+    respond_with @room
+  end
+  
+  def edit
+    respond_with @room
+  end
+  
+  def create
+    @room = Room.create(params[:room])
+
+    respond_with @room
+  end
+
+  def update
+    @room.update_attributes(params[:room])
+    
+    respond_with @room
+  end
+
+  def destroy
+    @room.destroy
+
+    respond_with @room
+  end
+  
+  private
+    def rooms
+      current_user.account.rooms
+    end
+    
+    def find_room
+      @room = rooms.find(params[:id])
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,4 @@ Owning::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
-  # Configure Faye to start along the Rails app in dev mode
-  config.middleware.use Faye::RackAdapter, :mount => '/faye', :timeout => 25
 end
