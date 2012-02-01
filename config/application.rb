24145b85b39226ff808eb4dc3b6352dbce93816f
macournoyer <macournoyer@gmail.com>
2012-02-01 12:16:37 -0500

    Introducte Faye middleware to get real time updates of messages.

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Owning
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.middleware.use Faye::RackAdapter, :mount      => '/faye',
+                                             :timeout    => 25
   end
 end
