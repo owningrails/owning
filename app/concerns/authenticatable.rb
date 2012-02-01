a5cb9bf0a3d427bf9e48d29a0281d95306a79e42
macournoyer <macournoyer@gmail.com>
2012-02-01 09:40:44 -0500

    Refactor auth logic out of User and into module.

---
+++ app/concerns/authenticatable.rb
@@ -0,0 +1,13 @@
+module Authenticatable
+  extend ActiveSupport::Concern
+  
+  included do
+    # Include default devise modules. Others available are:
+    # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable
+
+    # Setup accessible (or protected) attributes for your model
+    attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  end
+end
