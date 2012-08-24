23f37ba8236d942d8f5fe112f57e06c643ca7e9f
macournoyer <macournoyer@gmail.com>
2012-08-24 12:05:04 -0400

    Implement custom responder to display JS alert.

--- app/responders/js_error_catcher_responder.rb
+++ app/responders/js_error_catcher_responder.rb
@@ -1,6 +1,11 @@
 module JsErrorCatcherResponder
   def to_js
     ### Exercise: catch and display error.
-    super
+    if has_errors?
+      error_message = resource.errors.full_messages.to_sentence
+      render :js => %{alert("#{error_message}");}
+    else
+      super
+    end
   end
 end
